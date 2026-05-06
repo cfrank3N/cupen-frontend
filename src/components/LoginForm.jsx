e0c8af9dad7170e2b2abe984db27546ede4581b0
Adam Frank <adamcecarfrank@gmail.com>
2026-05-06 20:21:29 +0200

    Added login call to api

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,17 +1,49 @@
+import { useState } from "react";
 import { Button, Form, Container } from "react-bootstrap";
 
 function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const result = await fetch("http://localhost:8080/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+        credentials: "include",
+      });
+
+      const data = await result.json();
+      console.log(data);
+    } catch (err) {
+      console.log("Login failed", err);
+    }
+  };
+
   return (
     <Container className="my-5 d-flex justify-content-center align-items-cemter">
-      <Form>
+      <Form onSubmit={handleLogin}>
         <Form.Group className="mb-3" controlId="formBasicUsername">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="username" placeholder="Username" />
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Login
